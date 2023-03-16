2ad309d459954fb0d9e60000a4cb4b5b7099ce64
Everton <everton@devgrid.co.uk>
2023-03-16 16:23:49 -0300

    Initial Commit

---
+++ ChatGPTClient/ChatGPTClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTClientApp.swift
+//  ChatGPTClient
+//
+//  Created by Everton Carneiro on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
